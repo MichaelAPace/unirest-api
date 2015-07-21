ac2cdb102141863e466be43807028e5723da12df
MichaelAPace <mapace@gmail.com>
2015-07-21 11:07:48 -0600

    push to work on heroku?

--- app.js
+++ app.js
@@ -1,14 +1,99 @@
-require('dotenv').load()
+// require('dotenv').load()
+// var express = require('express');
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+//
+// var unirest = require('unirest');
+//
+//
+//
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
+//
+// var app = express();
+//
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+//
+// // uncomment after placing your favicon in /public
+// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// //NYT API BOOKS ROUTE
+// app.get('/books', function(req, res) {
+//     unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' +process.env.NYT_API_KEY)
+//       .end(function (response) {
+// //        console.log(response.body.results.books);
+//         var NYTBooks = response.body.results.books;
+//         // res.end('Done');
+//          res.render('index', {books:NYTBooks});
+//       })
+// })
+//
+//
+//
+// app.use('/', routes);
+// app.use('/users', users);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handlers
+//
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+//
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+//
+//
+// module.exports = app;
+
+
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var unirest = require('unirest');
-
-
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -27,38 +112,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//NYT API BOOKS ROUTE
+
 app.get('/books', function(req, res) {
-    unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' +process.env.NYT_API_KEY)
-      .end(function (response) {
-        console.log(response.body);
-        var NYTBooks = response.body.results.books;
+ unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
+     .end(function (response) {
+       console.log(response.body.results.books);
+         var NYTBooks = response.body.results.books;
+         res.render('index', {books: NYTBooks});
+         //res.end('Done')
+     })
 
-        // res.end('Done');
-         res.render('index', {books:NYTBooks});
-      })
 })
-
-
-
-
 
 app.use('/', routes);
 app.use('/users', users);
 
-
-
-
-
-
-
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+ var err = new Error('Not Found');
+ err.status = 404;
+ next(err);
 });
 
 // error handlers
@@ -66,23 +139,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+ app.use(function(err, req, res, next) {
+   res.status(err.status || 500);
+   res.render('error', {
+     message: err.message,
+     error: err
+   });
+ });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+ res.status(err.status || 500);
+ res.render('error', {
+   message: err.message,
+   error: {}
+ });
 });
 
 
