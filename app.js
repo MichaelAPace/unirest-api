a543e5a816652c52bbd67510d102af02125a65c9
MichaelAPace <mapace@gmail.com>
2015-07-21 10:36:14 -0600

    Render books from NYT API

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 
 var unirest = require('unirest');
 
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -30,7 +32,10 @@ app.get('/books', function(req, res) {
     unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' +process.env.NYT_API_KEY)
       .end(function (response) {
         console.log(response.body);
-        res.end('Done');
+        var NYTBooks = response.body.results.books;
+
+        // res.end('Done');
+         res.render('index', {books:NYTBooks});
       })
 })
 
